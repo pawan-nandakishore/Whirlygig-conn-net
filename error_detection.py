f8f5c74457ecc7f8f441812e684918f0c96c3fe5
pawan <pawan.nandakishore@gmail.com>
2017-04-04 18:44:28 +0530

    hopefully final

--- error_detection.py
+++ error_detection.py
@@ -63,7 +63,7 @@ def get_cropped_images_list(input_image, labeled,errval_all, list_of_centroids):
 
 def get_cropped_images_other(input_image,  new_list_of_centroids,list_cropped_images): 
     
-    for l in range(1,len(new_list_of_centroids)) : 
+    for l in range(0,len(new_list_of_centroids)) : 
 
         centroid = new_list_of_centroids[l]
         # crop image 
@@ -79,9 +79,9 @@ def get_cropped_images_other(input_image,  new_list_of_centroids,list_cropped_im
 def get_image_patch(input_image,list_cropped_images2,input_empty ): 
 
     stride =input_image.shape[1]/50 
-    stride = np.linspace(50, mask.shape[1]-50, 20)
+    stride = np.linspace(50, mask.shape[1]-50, 30)
     positions= []
-    print(stride)
+    # print(stride)
     stride = np.round(stride,0)
 
     for i in stride: 
@@ -91,7 +91,7 @@ def get_image_patch(input_image,list_cropped_images2,input_empty ):
     
     length_ij = len(positions)
 
-    print(len(list_cropped_images2))
+    # print(len(list_cropped_images2))
     ij = 0  
     for i in stride: 
         for j in stride: 
@@ -115,17 +115,7 @@ def get_image_patch(input_image,list_cropped_images2,input_empty ):
 
 
 def open_files(i,input_image):
- # For each image in the folder create mask  
-    # imageLoc = folderloc + i
-    # raw_imageLoc = raw_folder+raw_files[file_ind]
-    # labeled_imageLoc = labeled_folder+labeled_files[file_ind]
-    
-    
-    # image = imread(imageLoc, mode='RGB')
-    # raw_image = imread(raw_imageLoc, mode='RGB')
-    # labeled_image = imread(labeled_imageLoc, mode='RGB')
-    
-    # print(image.shape,raw_image.shape)
+ 
 
     mask = np.zeros(image.shape[0:2])
     
@@ -151,7 +141,7 @@ print(files, raw_files, labeled_files)
 file_ind = 0 
 
 print(files)
-all_cropped_images  = []
+result_cropped_images  = []
 raw_cropped_images = []
 labeled_cropped_images = []
 filecounter = 0 
@@ -159,7 +149,7 @@ filecounter = 0
 all_result_images = [ imread(folderloc+jimage, mode='RGB')  for jimage in files]
 all_raw_images = [ imread(raw_folder+rimage, mode='RGB')  for rimage in raw_files]
 all_labeled_images = [ imread(labeled_folder+limage, mode='RGB')  for limage in labeled_files]
-plt.close('all')
+plt.close()
    
 for single_img in range(0,len(all_result_images)):
     
@@ -196,16 +186,21 @@ for single_img in range(0,len(all_result_images)):
     empty_image  = np.ones(image.shape)*255
     empty_image2  = np.ones(image.shape)*255
     empty_image3  = np.ones(image.shape)*255
+    empty_image4  = np.ones(image.shape)*255
+    empty_image5  = np.ones(image.shape)*255
+    empty_image6  = np.ones(image.shape)*255
+    
     cropped_images =[]
     cropped_images2 =[]
     cropped_images3 = []
 
     
     #original cropped images
-    image = image-255 
+    image = image
 
     cropped_images,all_new_centroids = get_cropped_images_list(image, labeled,errval_all, list_of_centroids)
-    all_cropped_images.append(cropped_images) 
+    # print('len of cropped image: ', len(cropped_images))
+    result_cropped_images.append(cropped_images) 
     empty_image,positions  = get_image_patch(image,cropped_images,empty_image)
    
     
@@ -215,46 +210,47 @@ for single_img in range(0,len(all_result_images)):
    
 
 
-    sample_cropped = random.sample(cropped_images2,10)
-    print("all new cent:", len(all_new_centroids))
-    if(filecounter==1):
-        # f, axarr = plt.subplots(2, sharey = True)
-        # axarr[0].imshow(mask)
-        # for cent in range(len(all_new_centroids)):
-        #     centroid= all_new_centroids[cent] 
-        #     axarr[0].scatter(centroid[1],centroid[0],color ='red') 
-        
-        # axarr[1].imshow(raw_image)
-
-        # for cent in range(len(all_new_centroids)):
-        #     centroid= all_new_centroids[cent] 
-        #     axarr[1].scatter(centroid[1],centroid[0],color ='green') 
-        
-        # plt.show() 
-
-
-        # f, axarr = plt.subplots(3,3)
-        # imval  = 0 
-        # for k in range(0,3):
-        #     for k2 in range(0,3):
-        #         axarr[k,k2].imshow(sample_cropped[imval])
-        #         imval+=1
-        
-        plt.figure('raw')
-        plt.imshow(empty_image2)
-        plt.show()
-        f, axarr = plt.subplots(2, sharey = True)
-        axarr[0].imshow(empty_image)
-        axarr[1].imshow(empty_image2)
-        plt.show()
-        break
-    
     
     #labeled_cropped_images
 
     
     cropped_images3 = get_cropped_images_other(labeled_image,  all_new_centroids,cropped_images3)
     empty_image3,positions  = get_image_patch(labeled_image,cropped_images3,empty_image3)
+    
+    # sample_cropped = random.sample(cropped_images3,10)
+    # print("all new cent:", len(all_new_centroids))
+    # if(filecounter==1):
+    #     # f, axarr = plt.subplots(2, sharey = True)
+    #     # axarr[0].imshow(mask)
+    #     # for cent in range(len(all_new_centroids)):
+    #     #     centroid= all_new_centroids[cent] 
+    #     #     axarr[0].scatter(centroid[1],centroid[0],color ='red') 
+        
+    #     # axarr[1].imshow(raw_image)
+
+    #     # for cent in range(len(all_new_centroids)):
+    #     #     centroid= all_new_centroids[cent] 
+    #     #     axarr[1].scatter(centroid[1],centroid[0],color ='green') 
+        
+    #     # plt.show() 
+
+
+    #     # f, axarr = plt.subplots(3,3)
+    #     # imval  = 0 
+    #     # for k in range(0,3):
+    #     #     for k2 in range(0,3):
+    #     #         axarr[k,k2].imshow(sample_cropped[imval])
+    #     #         imval+=1
+        
+    #     plt.figure('labeled')
+    #     plt.imshow(empty_image3/255)
+    #     plt.show()
+    #     f, axarr = plt.subplots(2, sharey = True)
+    #     axarr[0].imshow(empty_image)
+    #     axarr[1].imshow(empty_image2/255)
+    #     plt.show()
+    #     break
+    
     
 
     
@@ -263,7 +259,6 @@ for single_img in range(0,len(all_result_images)):
     
     filecounter += 1 
 
-    # placing the patches in empty image 
 
 
 # sample_cropped = random.sample(cropped_images,10)
@@ -277,13 +272,55 @@ for single_img in range(0,len(all_result_images)):
 
 plt.show()
 
+
+
 print('len of labeled: ',len(np.array(labeled_cropped_images)))
 
 print('len of raw: ',len(np.array(raw_cropped_images)))
 
-print('len of image: ',len(np.array(all_cropped_images)))
+print('len of image: ',len(np.array(result_cropped_images)))
 
 
+
+labeled_cropped_images2 = [  y  for x in labeled_cropped_images for y in x]
+raw_cropped_images2 = [  y  for x in raw_cropped_images for y in x]
+result_cropped_images2 =[  y  for x in result_cropped_images for y in x]
+
+print(len(labeled_cropped_images2), len(raw_cropped_images2), len(result_cropped_images2))
+if(len(result_cropped_images2) ==len(raw_cropped_images2) ): 
+    print('Number of result images matches the number of raw images generated, hence all is good  ')
+
+zippeddata = zip(labeled_cropped_images2,raw_cropped_images2, result_cropped_images2 )
+# len_data = len(result_cropped_images2)
+# sampled_cropped_images  = random.sample(zippeddata,int(len_data)) 
+labeled_images_sampled, raw_images_sampled, result_images_sampled = zip(*zippeddata)
+
+
+f, axarr = plt.subplots(3, sharey = True)
+axarr[0].imshow(labeled_images_sampled[55])
+axarr[1].imshow(raw_images_sampled[55])
+axarr[2].imshow(result_images_sampled[55])
+plt.show()       
+
+# empty image 4 displays tiles where each tile is centered around a whirlygig which has an error in its detection
+# empty image 5 and 6 correspond to the raw and labeled image for each
+
+
+
+empty_image4,positions  = get_image_patch(image,result_images_sampled,empty_image4)
+empty_image5,positions  = get_image_patch(raw_image,raw_images_sampled,empty_image5)
+empty_image6,positions  = get_image_patch(labeled_image,labeled_images_sampled,empty_image6)
+
+f, axarr = plt.subplots(3, sharey = True)
+axarr[0].imshow(empty_image4-255)
+axarr[1].imshow(empty_image5)
+axarr[2].imshow(empty_image6/255)
+plt.show()       
+  
+
+
+
+# print(len(labeled_cropped_images[0]),len(labeled_cropped_images[1]),len(labeled_cropped_images[2]),len(labeled_cropped_images[3]),len(labeled_cropped_images[4]) )
 
  # empty_image,positions  = get_image_patch(image_type,cropped_images )
     
