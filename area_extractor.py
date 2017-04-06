25e1ae91b795a09db3057794d7339ecdad66f7bb
Shashwat <thegreatshasha@gmail.com>
2017-04-06 17:33:03 +0530

    add area extractor with __main__

--- area_extractor.py
+++ area_extractor.py
@@ -32,50 +32,51 @@ def get_regions(image):
     return regions, areas
     #regions_g.extend(region)
 
-results = glob.glob('plots/results/*')
+if __name__ == "__main__":
+    results = glob.glob('plots/results/*')
 
-areas_g = []
+    areas_g = []
 
-for fl in results:
-    image = imread(fl)
-    regions, areas = get_regions(image)
-    areas_g.extend(areas)
+    for fl in results:
+        image = imread(fl)
+        regions, areas = get_regions(image)
+        areas_g.extend(areas)
 
-areas_g = np.array(areas_g)
-areas_g.shape
-mean, std, left, right = calculate_stats(areas_g)
-plt.hist(areas_g, bins=20)
-plt.show()
-
-for fl in results:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    image = imread(fl)
-    image_bw = get_blue_region(image)
-    #con_hull = convex_hull_object(image_bw)
-    label_image = label(image_bw)
-    regions = regionprops(label_image)
-    #plt.imshow(label_image)
-    #plt.show()
-    #print(image.shape, label_image.shape)
-    #image_label_overlay = label2rgb(label_image, image)
-    ax.imshow(image_bw, cmap='Greys')
-    #plt.show()
-    #regions, areas = get_regions(image_bw)
-
-    for r in regions:
-       if lies_outside(r.solidity, mean, std, left, right):
-           minr, minc, maxr, maxc = r.bbox
-           rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
-           ax.add_patch(rect)
-
-    ax.set_axis_off()
-    plt.tight_layout()
+    areas_g = np.array(areas_g)
+    areas_g.shape
+    mean, std, left, right = calculate_stats(areas_g)
+    plt.hist(areas_g, bins=20)
     plt.show()
 
-#ax.set_axis_off()
-#plt.tight_layout()
-#plt.show()
+    for fl in results:
+        fig, ax = plt.subplots(figsize=(10, 6))
+        image = imread(fl)
+        image_bw = get_blue_region(image)
+        #con_hull = convex_hull_object(image_bw)
+        label_image = label(image_bw)
+        regions = regionprops(label_image)
+        #plt.imshow(label_image)
+        #plt.show()
+        #print(image.shape, label_image.shape)
+        #image_label_overlay = label2rgb(label_image, image)
+        ax.imshow(image_bw, cmap='Greys')
+        #plt.show()
+        #regions, areas = get_regions(image_bw)
 
-# print(mean, std, left, right)
+        for r in regions:
+        if lies_outside(r.solidity, mean, std, left, right):
+            minr, minc, maxr, maxc = r.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
+            ax.add_patch(rect)
+
+        ax.set_axis_off()
+        plt.tight_layout()
+        plt.show()
+
+    #ax.set_axis_off()
+    #plt.tight_layout()
+    #plt.show()
+
+    # print(mean, std, left, right)
 
 
