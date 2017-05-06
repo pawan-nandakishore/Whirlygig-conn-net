9785d26edbf8841b220faa1690d273bfeeb46224
monisha <thegreatshasha@gmail.com>
2017-05-06 23:06:39 +0530

    commit ugly output code again

--- output_multiple_crop.py
+++ output_multiple_crop.py
@@ -61,11 +61,11 @@ if __name__ == "__main__":
         model = load_model(model_n, custom_objects={'your_loss': your_loss})
         print("Loaded :%s", model_n)
     
-        files_all = glob.glob('cleaned/rgbs/*.png')
+        files_all = glob.glob('images/rgbs/*.png')
         #files_all = glob.glob('images/single_frames/*.png')
         #files_all = glob.glob('images/all_grey/15_jpgs/*.jpg')
         #files_all = random.sample(files_all)
-        #files_all = glob.glob('cleaned/penta/*')
+        #files_all = glob.glob('images/penta/*')
         #files = files+files+files# + files[-4:-1]
         #print(files)
     
@@ -97,26 +97,27 @@ if __name__ == "__main__":
                     #imgcount = 0
                     count= 0
                     img = imgs[ix]
+                    outputs = np.zeros((img.shape[0], img.shape[1], 4))
                     zeros = np.zeros((img.shape[0],img.shape[1],4))
     
                     for i in xrange(0, img.shape[0], inner_size):
                         for j in xrange(0, img.shape[1], inner_size):
-                            zeros[i:i+inner_size,j:j+inner_size] = y[count]
+                            outputs[i:i+inner_size,j:j+inner_size] = y[count]
                             count += 1
                     
                     for i in range(img.shape[0]):
                         for j in range(img.shape[1]):
-                            output = zeros[i,j]
+                            output = outputs[i,j]
                             zeros[i,j,np.argmax(output)] = 1
                             #count += 1
     
                     zeros[:,:,3]=1
     
-                    color = output_to_colors(zeros, img)
+                    #color = output_to_colors(zeros, img)
     
                     #colors = [output_to_colors(y, imgs[i]) for i,y in enumerate(ys)]
                     #colors = [label2rgb(y.argmax(axis=-1), image=imgs[i], colors=[(1,0,0), (0,1,0), (0,0,1), (0,0,0)], alpha=0.9, bg_label=3) for i,y in enumerate(ys)]
     
                     #[plt.imsave('plots/%s_%s'%(model_n, file_names[i]), zeros) for i,zeros in enumerate(colors)]
                     print(file_names)
-                    plt.imsave('plots/results/%s.png'%(file_names[ix]), color)
+                    plt.imsave('plots/results/%s.png'%(file_names[ix]), zeros)
