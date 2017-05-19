3b75f2450b47e23af8813a3521461069598fb596
monisha <thegreatshasha@gmail.com>
2017-05-19 13:52:41 +0530

    back to older encoder

--- models.py
+++ models.py
@@ -194,29 +194,6 @@ def keras_mnist_autoencoder(img_shape=(28, 28, 1)):
     input_img = Input(img_shape)
     x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    encoded = MaxPooling2D((2, 2), padding='same')(x)
-    
-    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
-    
-    x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu')(x)
-    x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-    
-    autoencoder = Model(input_img, decoded)
-    return autoencoder
-
-def keras_mnist_autoencoder_loaded(img_shape=(28, 28, 1)):
-    """ Copy pasted autoencoder from keras with lot more parameters to use as a benchmark. """
-    input_img = Input(img_shape)
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-    x = MaxPooling2D((2, 2), padding='same')(x)
     x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
     x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
@@ -227,6 +204,29 @@ def keras_mnist_autoencoder_loaded(img_shape=(28, 28, 1)):
     x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    
+    autoencoder = Model(input_img, decoded)
+    return autoencoder
+
+def keras_mnist_autoencoder_loaded(img_shape=(28, 28, 1)):
+    """ Copy pasted autoencoder from keras to use as a benchmark. """
+    input_img = Input(img_shape)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
+    
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+    
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(16, (3, 3), activation='relu')(x)
     x = UpSampling2D((2, 2))(x)
